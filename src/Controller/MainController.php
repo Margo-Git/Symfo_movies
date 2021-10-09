0cc0762286494a32e23834f5e4f4a189a6722212
Margo-Git <margobrenneur@gmail.com>
2021-10-09 13:41:42 +0200

    requete custom casting d'un film joint sur person

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Movie;
 use App\Repository\MovieRepository;
+use App\Repository\CastingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,16 +30,20 @@ class MainController extends AbstractController
      *
      * @Route("movie/{id<\d+>}", name="movie_show")
      */
-    public function movieShow(Movie $movie = null)
+    public function movieShow(CastingRepository $castingRepository, Movie $movie = null)
     {
         // film non trouvé
         if ($movie === null) {
             throw $this->createNotFoundException('Film non trouvé');
         }
 
+        // on utilise notre requête custom
+        $castings = $castingRepository->findAllCastingsByMovieJoinedToPersonDQL($movie->getId());
+
         
         return $this->render('main/movie_show.html.twig', [
-            'movie' => $movie
+            'movie' => $movie,
+            'castings' => $castings,
         ]);
     }
 }
--- src/Repository/CastingRepository.php
+++ src/Repository/CastingRepository.php
@@ -19,32 +19,30 @@ class CastingRepository extends ServiceEntityRepository
         parent::__construct($registry, Casting::class);
     }
 
-    // /**
-    //  * @return Casting[] Returns an array of Casting objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Casting
+    /**
+     * Tous les castings d'un film donné
+     * joints sur l'entité Person
+     * 
+     * SELECT * FROM `casting`
+     * INNER JOIN `person` ON `casting`.`person_id` = `person`.`id`
+     * WHERE `movie_id` = 7
+     */
+    public function findAllCastingsByMovieJoinedToPersonDQL(int $movieId)
     {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT c, p
+            -- je veux récupérer des données de person depuis l\'entité casting
+            FROM App\Entity\Casting c
+            INNER JOIN c.person p
+            WHERE c.movie = :id
+            ORDER BY c.creditOrder'
+        )->setParameter('id', $movieId);
+
+        return $query->getResult();
     }
-    */
+    
+
 }
